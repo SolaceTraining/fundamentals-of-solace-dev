424c3241ff49b4638836482019be1b19b1e08ab9
MaluTom <madmaludawg@gmail.com>
2018-11-01 16:20:38 -0400

    Updating REST Post to include CORS

--- src/main/resources/static/scripts/solaceauth.js
+++ src/main/resources/static/scripts/solaceauth.js
@@ -11,7 +11,7 @@
 function authenticate(sUsername, sPassword, oResultCallback) {
     'use strict';
 
-    var serverUrl = 'http://localhost:8081/solace/cloud/proxy',
+    var serverUrl = 'http://localhost:8082/solace/cloud/proxy',
         jsonBody = {
             username: sUsername,
             password: sPassword
@@ -19,7 +19,9 @@ function authenticate(sUsername, sPassword, oResultCallback) {
 
     fetch(serverUrl, {
         method: "POST",
-        body: JSON.stringify(jsonBody)
+        body: JSON.stringify(jsonBody),
+        mode: "cors",
+        cache: "no-cache"
     })
         .then(function (response) {
             console.debug(response);

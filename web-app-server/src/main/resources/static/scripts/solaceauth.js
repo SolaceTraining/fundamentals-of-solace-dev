adc18d372b945346cd5d7e65a69679f49076faf1
MaluTom <madmaludawg@gmail.com>
2018-11-02 13:39:19 -0400

    Removing Cors

--- web-app-server/src/main/resources/static/scripts/solaceauth.js
+++ web-app-server/src/main/resources/static/scripts/solaceauth.js
@@ -11,7 +11,7 @@
 function authenticate(sUsername, sPassword, oResultCallback) {
     'use strict';
 
-    var serverUrl = 'http://localhost:8082/solace/cloud/proxy',
+    var serverUrl = 'http://localhost:8081/solace/cloud/proxy',
         jsonBody = {
             username: sUsername,
             password: sPassword
@@ -19,8 +19,6 @@ function authenticate(sUsername, sPassword, oResultCallback) {
 
     fetch(serverUrl, {
         method: "POST",
-        mode: "cors",
-        cache: "no-cache",
         body: JSON.stringify(jsonBody)
     })
         .then(function (response) {

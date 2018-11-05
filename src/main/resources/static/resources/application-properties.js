dab6ecb16377f327020a5f88968b37cd0d197d4a
MaluTom <madmaludawg@gmail.com>
2018-11-05 16:53:12 -0500

    Fixing gitignore

--- src/main/resources/static/resources/application-properties.js
+++
@@ -1,9 +0,0 @@
-connectOptions = {
-	sBROKERURL: 'ws://mr85s7yb497x.messaging.solace.cloud:20067',
-	sVPN: 'msgvpn-85s7ybd8c5',
-	sUSERNAME: 'solace-cloud-client',
-	sPASSWORD: 'g41cea9klv3u6j71kq0sigkuk9',
-	sPublishTopic: 'solace/chat',
-	sSubscribeTopic: 'solace/chat',
-	sReceiveQueue: 'CHAT_QUEUE'
-}
